0af9676cb0d3f8e6a80e6c9f2e471dd7804773a7
Andy Woodruff <awoodruff@gmail.com>
2015-03-24 16:15:48 -0400

    Basic grid of maps on category click

---
+++ js/category.js
@@ -0,0 +1,21 @@
+var selectedCategory;
+
+function selectCategory( id ){
+	$( "body" ).attr( "class", "category-screen " + id );
+	selectedCategory = id;
+	showMapsInCategory( id );
+}
+
+function showMapsInCategory( id ){
+	var $mapsDiv = $( "#map-cards" );
+	
+	maps.data.forEach( function( m, i ){
+		if ( m.category != id ) return;
+		var $div = $( "<div>" ).appendTo( $mapsDiv )
+			.attr( "class", "map-card card" )
+			.attr( "id", "map" + m.number )
+		$( "<div><p>" + m.title + "</p></div>" )
+			.css( "border-top-color", colors[i] )
+			.appendTo( $div );
+	});
+}
