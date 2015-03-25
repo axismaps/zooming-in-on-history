2591cec2d7f2ce2a716f84349b53fe32bcbd06c0
Andy Woodruff <awoodruff@gmail.com>
2015-03-25 18:08:08 -0400

    Shrink home screen cards a bit; make bouncy

--- js/home.js
+++ js/home.js
@@ -5,13 +5,32 @@ function createCategories(){
 	categories.data.forEach( function( cat, i ){
 		if ( params.categories.indexOf( cat.id ) == -1 ) return;	// somehow this needs to know which categories to show
 		var $div = $( "<div>" ).appendTo( $categoriesDiv )
-			.attr( "class", "category card " + cat.id )
+			.attr( "class", "category card " + cat.id  )
 			.attr( "id", cat.id )
 			.click( function(){
 				selectCategory( cat.id );
+			})
+			.on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+				$(this).removeClass( "animated" ).removeClass( "bounce" ).removeClass( "bounceInDown" );
 			});
 		$( "<div><p>" + cat.name + "</p></div>" )
 			.css( "border-top-color", colors[i] )
 			.appendTo( $div );
+
+		var animCount = 1;
+
+		setTimeout( function(){
+			setInterval( function(){
+				if ( animCount < 3 ){
+					$div.addClass( "animated bounce" );
+					animCount ++;
+				} else {
+					$div.addClass( "animated bounceInDown" );
+					animCount = 1;
+				}
+			},10000)
+		},i*1000)
 	});
+
+	
 }
