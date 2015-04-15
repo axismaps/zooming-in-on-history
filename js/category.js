69dd9df05f26857d7050c118443b8e889ee80d37
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-04-15 12:15:13 -0500

    Remove buggy animation for now

--- js/category.js
+++ js/category.js
@@ -37,7 +37,12 @@ function showMapsInCategory( id ){
 			.addClass( "map-card card page" + pageCount + " pre-animated" + ( i % 4 + 1 ) )
 			.css( "background-image", "url( data/img/thumbnails/" + m.number + ".jpg )" )
 			.click( function(){
-				showDetailsForMap( m.number, false, false );
+				//if on mobile, adjust animation settings
+				if( $( window ).width() <= 767 ) {
+					showDetailsForMap( m.number, false, true );
+				} else {
+					showDetailsForMap( m.number, false, false );
+				}
 				breadcrumbCSSUpdates();
 			});
 		
